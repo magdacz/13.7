0af1e8831914e8b59a70d0d15ee1c654e44b0b40
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2018-05-31 22:59:21 +0200

    Add files

---
+++ index.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var colors = require('colors');
+var StatMode = require('stat-mode');
+
+/*
+fs.stat('cat.jpeg', function(err, stats) {
+    var statMode = new StatMode(stats);
+    console.log(statMode.toString());
+});
+
+fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+    console.log('Dane przed zapisem!'.blue);
+    console.log(data);
+    fs.appendFile('./tekst.txt', '\nA tak wyglądają po zapisie!', function(err) {
+        if (err) throw err;
+        console.log('Zapisano!'.blue);
+        fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+            console.log('Dane po zapisie'.blue)
+            console.log(data);
+        });
+    });
+}); 
+*/
+
+fs.readdir('./katalog', 'utf-8' function(err, files) {
+    if (err) throw err;
+    console.log(files);
+    
+    fs.writeFile('./tekst.txt', function(err) {
+        if (err) throw err;
+        console.log('Zapisano!'.green);
+    }); 
+}) 
+
+
